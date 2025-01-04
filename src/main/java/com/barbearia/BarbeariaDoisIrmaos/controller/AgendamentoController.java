57a703933257993f4b2d57ad0890141bd690ef3c
“Jarmeson <jarmesonvini@gmail.com>
2025-01-04 20:59:44 -0300

    criação do método que recebe os dados do formulário e salva em uma lista

--- src/main/java/com/barbearia/BarbeariaDoisIrmaos/controller/AgendamentoController.java
+++ src/main/java/com/barbearia/BarbeariaDoisIrmaos/controller/AgendamentoController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller //precisa especificar que é uma classe de controler para que ele entenda que é de controle e não uma classe normal
 public class AgendamentoController {
@@ -27,6 +29,50 @@ public class AgendamentoController {
         model.addAttribute("agendamento", new Agendamento()); //criando um objeto de agendamento(Agendamento) vazio com seus atributos e jogando para a model.
         return "cadastro"; //procurar pelo HTML com nome de: cadastro
     }
+    
+    @PostMapping("/gravar")
+    public String processarFormulario(@ModelAttribute Agendamento agendamento, Model model){ //receber os dados do formulário e salva numa lista de agendamentos
+        if (agendamento.getId() != null){ //se for diferente de nulo, ele atualiza
+            for(Agendamento a: listaAgendamentos){
+            if(a.getId()== agendamento.getId()){ //esse id se refere ao id hidden (campo invisível) do HTML cadastro. 
+                a.setNome(agendamento.getNome());
+                a.setTelefone(agendamento.getTelefone());
+                a.setData(agendamento.getData());
+                a.setHora(agendamento.getHora());
+                a.setBarbeiro_resp(agendamento.getBarbeiro_resp());
+                a.setCabelo(agendamento.getCabelo());
+                a.setBarba(agendamento.getBarba());
+                a.setSobrancelha(agendamento.getSobrancelha());
+                a.setHidratacao(agendamento.getHidratacao());
+                a.setTintura(agendamento.getTintura());
+                
+                break;
+            }
+        }
+            
+        }
+        else{
+            agendamento.setId(listaAgendamentos.size() + 1); //adicionando ID
+//            livro.setLido(false); //informando que ele não foi lido (informação default)
+            listaAgendamentos.add(agendamento); //INSERIR NA LISTA
+        }
+        return "redirect:/listagem"; //chamar a listagem dos agendamentos salvos
+    }
+    
+        @GetMapping("/listagem")
+        public String listaForm(Model model){
+            model.addAttribute("lista", listaAgendamentos);
+            return "listagem";
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
