280737cd567960210980065faecbe71844bb12a7
“Jarmeson <jarmesonvini@gmail.com>
2025-01-04 13:46:23 -0300

    Configuradas as pastas do Projeto com base no Spring MVC

---
+++ src/main/java/com/barbearia/BarbeariaDoisIrmaos/BarbeariaDoisIrmaosApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.BarbeariaDoisIrmaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaDoisIrmaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaDoisIrmaosApplication.class, args);
+	}
+
+}
